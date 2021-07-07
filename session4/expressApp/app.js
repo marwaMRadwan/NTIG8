fa7d0e9a3ff2633714cc5475e8791861612cb84c
marwa Radwan <marwaradwan872@hotmail.com>
2021-07-07 11:29:59 +0200

    s4

---
+++ session4/expressApp/app.js
@@ -0,0 +1,28 @@
+const express= require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT
+
+const myPublicDir = path.join(__dirname, 'public')
+app.use(express.static(myPublicDir))
+
+const myViewsDir = path.join(__dirname, 'template/views')
+app.set('view engine', 'hbs')
+app.set('views', myViewsDir)
+// console.log(path.join(__dirname,'x.html'))
+//res.send(any data)
+// app.get('', (req, res)=>{
+//     data = {a:4}
+//     res.sendFile(path.join(__dirname,'x.html'))
+// })
+
+app.get('', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/x/y', (req, res)=>{
+    res.render('home')
+})
+
+
+app.listen(PORT, ()=> console.log(`hello on localhost:${PORT}`))
