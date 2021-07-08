676d0ef698c015477f71cdc3e221c2ddd7fdbd30
marwa Radwan <marwaradwan872@hotmail.com>
2021-07-08 13:56:36 +0200

    s5

--- session5/controller/user.controller.js
+++ session5/controller/user.controller.js
@@ -1,92 +1,89 @@
 const userModel = require('../models/user.model')
-class x {
-    add = async(req,res)=>{
-        try{
-            const userData = new userModel(req.body)
-            await userData.save()
-            res.status(200).send({
-                apiStatus: true,
-                message: " data inserted",
-                data: userData
-            })
-        }
-        catch(e){
-            res.status(500).send({
-                apiStatus:false,
-                message: "error inserting data",
-                data: e
-            })
-        }
+add = async(req,res)=>{
+    try{
+        const userData = new userModel(req.body)
+        await userData.save()
+        res.status(200).send({
+            apiStatus: true,
+            message: " data inserted",
+            data: userData
+        })
     }
-    allUsers=  async(req,res)=>{
-        try{
-            const data = await userModel.find()
-            res.status(200).send({
-                apiStatus:true,
-                message:"data retrived",
-                data: data,
-                count: data.length
-            })
-        }
-        catch(e){
-            res.status(500).send({
-                apiStatus:false,
-                message:"error loading data",
-                data: e
-            })
-        }
-    }
-    singleUser = async(req,res)=>{
-        try{
-            const id = req.params.id
-            const data = await userModel.findById(id)
-            if(!data){
-                res.status(404).send({
-                    apiStatus:true,
-                    message:"user not found",
-                    data: null
-                })
-            }
-            res.status(200).send({
-                apiStatus:true,
-                message:"data retrived",
-                data: data
-            })
-        }
-        catch(e){
-            res.status(500).send({
-                apiStatus:false,
-                message:"error loading data",
-                data: e
-            })
-        }
-    }
-    deleteuser = async(req,res)=>{
-        try{
-            const id = req.params.id
-            const data = await userModel.findByIdAndDelete(id)
-            if(!data){
-                res.status(404).send({
-                    apiStatus:true,
-                    message:"user not found",
-                    data: null
-                })
-            }
-            res.status(200).send({
-                apiStatus:true,
-                message:"data deleted",
-                data: data
-            })
-        }
-        catch(e){
-            res.status(500).send({
-                apiStatus:false,
-                message:"error loading data",
-                data: e
-            })
-        }
+    catch(e){
+        res.status(500).send({
+            apiStatus:false,
+            message: "error inserting data",
+            data: e
+        })
     }
 }
-
+allUsers=  async(req,res)=>{
+    try{
+        const data = await userModel.find()
+        res.status(200).send({
+            apiStatus:true,
+            message:"data retrived",
+            data: data,
+            count: data.length
+        })
+    }
+    catch(e){
+        res.status(500).send({
+            apiStatus:false,
+            message:"error loading data",
+            data: e
+        })
+    }
+}
+singleUser = async(req,res)=>{
+    try{
+        const id = req.params.id
+        const data = await userModel.findById(id)
+        if(!data){
+            res.status(404).send({
+                apiStatus:true,
+                message:"user not found",
+                data: null
+            })
+        }
+        res.status(200).send({
+            apiStatus:true,
+            message:"data retrived",
+            data: data
+        })
+    }
+    catch(e){
+        res.status(500).send({
+            apiStatus:false,
+            message:"error loading data",
+            data: e
+        })
+    }
+}
+deleteuser = async(req,res)=>{
+    try{
+        const id = req.params.id
+        const data = await userModel.findByIdAndDelete(id)
+        if(!data){
+            res.status(404).send({
+                apiStatus:true,
+                message:"user not found",
+                data: null
+            })
+        }
+        res.status(200).send({
+            apiStatus:true,
+            message:"data deleted",
+            data: data
+        })
+    }
+    catch(e){
+        res.status(500).send({
+            apiStatus:false,
+            message:"error loading data",
+            data: e
+        })
+    }
+}
 
 module.exports = { add, allUsers, singleUser, deleteuser }
