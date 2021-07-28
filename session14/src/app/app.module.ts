4da8caba882b7ba878dca94624411f320ace1f9c
marwa Radwan <marwaradwan872@hotmail.com>
2021-07-28 14:06:03 +0200

    s14

--- session14/src/app/app.module.ts
+++ session14/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,6 +37,7 @@ import { Error404Component } from './pages/error404/error404.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- session14/src/app/pages/users/profile/profile.component.ts
+++ session14/src/app/pages/users/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+contactus = new FormGroup({
+  name:new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]), //Validators.pattern('')
+  email:new FormControl(),
+  message:new FormControl(),
+  phone:new FormControl(),
 
+})
+get name(){ return this.contactus.get('name')}
   constructor() { }
 
   ngOnInit(): void {
+  }
+  handleSubmit(){
+    console.log(this.contactus.valid);
+    
+    console.log(this.contactus.value)
   }
 
 }
