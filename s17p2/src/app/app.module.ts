7242b8980398a1f3b74dda97ddaedc69f3b144c9
marwa Radwan <marwaradwan872@hotmail.com>
2021-08-02 14:43:14 +0200

    s17

--- s17p2/src/app/app.module.ts
+++ s17p2/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, HomeModule
+    AppRoutingModule, 
+    HomeModule
     ],
   providers: [],
   bootstrap: [AppComponent]
