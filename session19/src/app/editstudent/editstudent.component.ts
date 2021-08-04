66fda73acde89f9dc0f966266c07592b0bad07cc
marwa Radwan <marwaradwan872@hotmail.com>
2021-08-04 14:41:41 +0200

    s19

--- session19/src/app/editstudent/editstudent.component.ts
+++ session19/src/app/editstudent/editstudent.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CrudService } from '../shared/crud.service';
 
 @Component({
   selector: 'app-editstudent',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditstudentComponent implements OnInit {
 
-  constructor() { }
+  isSubmitted : boolean = false
+  studentForm= new FormGroup({
+    firstName:new FormControl('', [Validators.required, Validators.minLength(5)]),
+    lastName:new FormControl('',[Validators.required, Validators.minLength(5)]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    mobileNumber:new FormControl('', [Validators.required, Validators.minLength(11),Validators.maxLength(11)])
+  })
+  constructor( private _crud: CrudService, private _router:Router, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this._activatedRoute.snapshot.params.id
+    this._crud.GetSingleStudent(id).valueChanges().subscribe(data=>{
+      this.studentForm.setValue(data)
+    })
+  }
+  get firstName(){return this.studentForm.get('firstName')}
+  get lastName(){return this.studentForm.get('lastName')}
+  get email(){return this.studentForm.get('email')}
+  get mobileNumber(){return this.studentForm.get('mobileNumber')}
+
+  resstMyForm(){
+    this.studentForm.reset()
+  }
+  UpdateStudent(){
+    this.isSubmitted=true
+    if(this.studentForm.valid){
+      this._crud.UpdateStudent(this.studentForm.value)
+      this._router.navigateByUrl('/allStudents')
+    }
   }
 
 }
