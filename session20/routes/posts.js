6fb18404815c9f1c937e640ee9d100bac03eca98
marwa Radwan <marwaradwan872@hotmail.com>
2021-08-05 11:13:59 +0200

    s20

--- session20/routes/posts.js
+++ session20/routes/posts.js
@@ -7,42 +7,42 @@ router.get('/posts', async(req,res)=>{
     res.send(posts)
 })
 
-// router.post('/posts', async(req,res)=>{
-//     const post = new Post(req.body)
-//     await post.save()
-//     res.send(post)
-// })
+router.post('/posts', async(req,res)=>{
+    const post = new Post(req.body)
+    await post.save()
+    res.send(post)
+})
 
-// router.get('/posts/:id', async(req,res)=>{
-//     try{
-//     const posts = await Post.findOne({_id:req.params.id})
-//     res.send(posts)
-//     }
-//     catch(e){
-//         res.send({err:"not found"})
-//     }
-// })
+router.get('/posts/:id', async(req,res)=>{
+    try{
+    const posts = await Post.findOne({_id:req.params.id})
+    res.send(posts)
+    }
+    catch(e){
+        res.send({err:"not found"})
+    }
+})
 
-// router.patch('/posts/:id', async(req,res)=>{
-//     try{
-//     const post = await Post.findOne({_id:req.params.id})
-//     if(req.body.title) post.title = req.body.title
-//     if(req.body.content) post.title = req.body.content
-//     await post.save()
-//     res.send(post)
-//     }
-//     catch(e){
-//         res.send({err:"not found"})
-//     }
-// })
+router.patch('/posts/:id', async(req,res)=>{
+    try{
+    const post = await Post.findOne({_id:req.params.id})
+    if(req.body.title) post.title = req.body.title
+    if(req.body.content) post.title = req.body.content
+    await post.save()
+    res.send(post)
+    }
+    catch(e){
+        res.send({err:"not found"})
+    }
+})
 
-// router.delete('/posts/:id', async(req,res)=>{
-//     try{
-//     await Post.deleteOne({_id:req.params.id})
-//     res.send({})
-//     }
-//     catch(e){
-//         res.send({err:"not found"})
-//     }
-// })
+router.delete('/posts/:id', async(req,res)=>{
+    try{
+    await Post.deleteOne({_id:req.params.id})
+    res.send({})
+    }
+    catch(e){
+        res.send({err:"not found"})
+    }
+})
 module.exports= router
--- session20/tests/server.test.js
+++ session20/tests/server.test.js
@@ -33,3 +33,34 @@ test("GET /api/posts", async()=>{
         
     })
 })
+
+test("POST /api/posts", async()=>{
+    const data = {title:"t1", content:"c1"}
+    await supertest(app).post("/api/posts")
+    .send(data)
+    .expect(200)
+    .then(async(response)=>{
+        expect(response.body._id).toBeTruthy()
+        expect(response.body.title).toBe(data.title)
+        expect(response.body.content).toBe(data.content)
+
+        const post = await Post.findOne({_id: response.body._id})
+        expect(post).toBeTruthy()
+        expect(post.title).toBe(data.title)
+        expect(post.content).toBe(data.content)
+    })
+})
+
+test("GET /api/posts/:id", async()=>{
+    const post = await Post.create({title:"p 1", content:'c 1'})
+    await supertest(app).get("/api/posts/"+post.id)
+    .expect(200)
+    .then((response)=>{
+        expect(response.body._id).toBe(post.id)
+        expect(response.body.title).toBe(post.title)
+        expect(response.body.content).toBe(post.content)
+    })
+  
+})
+//patch('/posts/:id'
+// delete('/posts/:id'
